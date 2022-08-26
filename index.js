451c2d1d5dfe46a0534c7135d5e5086da0b7b72c
Stanley Masinde <stanmasinde@gmail.com>
2022-08-26 16:53:37 +0300

    part 2.1: Route parameters

--- index.js
+++ index.js
@@ -5,7 +5,36 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
+const users = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john@example.com"
+    },
+    {
+        id: 2,
+        name: "Jane Doe",
+        email: "jane@example.com"
+    }
+]
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id
+    const user = users.find(user => {
+        return user.id == userId
+    })
+
+    if(user) {
+        return res.json(user)
+    }
+
+    return res.status(404).json('User not found')
+})
 
 app.listen(3000, () => {
     console.log('application running on http://localhost:3000')
 })
+
