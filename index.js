d4e5fbafbab3237f8aa6568519a907800d3060c2
Stanley Masinde <stanmasinde@gmail.com>
2022-08-27 15:40:29 +0300

    Introduction to templating with pug

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 
 const app = express()
+app.set('view engine', 'pug')
+
 const usersRouter = require('./routes/users')
 
+// The index route 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.render('index', {
+        title: "Hello awesome person?"
+    })
+})
+
+// Respond with a user's name passed as a param
+app.get('/:name', (req, res) => {
+    const name = req.params.name
+
+    res.render('index', {
+        title: `Hello ${name}?`
+    })
 })
 
 app.use('/users', usersRouter)
